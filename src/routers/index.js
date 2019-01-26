c0af08c6021cad3c2b5724ee371f864c0e105b15
gummelhummel <fabian.els@outlook.com>
2019-01-26 20:34:14 +0100

    cache it

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,10 @@
 const bodyParser = require("body-parser");
-const cache = require("express-redis-cache")({ expire: 60 });
-var httpProxy = require("http-proxy");
+const cache = require("express-redis-cache")({
+  host: "localhost",
+  port: "6379"
+});
+const https = require("https");
 const path = require("path");
-const request = require("request");
-
-var proxy = httpProxy.createProxyServer({expire:60}); // See (†)
 
 module.exports = (express, services) => {
   const apiDataRouter = (() => {
@@ -47,35 +47,16 @@ module.exports = (express, services) => {
 
       app.use("/api/data", apiDataRouter);
 
-      //      https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-      app.use(
-        "/map/:s/:z/:x/:y",
-        cache.route({}), (req, res) => {
-          let p = req.params;
-          let url = `https://${p.s}.tile.openstreetmap.org/${p.z}/${p.x}/${p.y}.png`;
-          console.log('so isses' + url);
+      app.use("/map/:s/:z/:x/:y", cache.route(), (req, res) => {
+        let p = req.params;
 
-          const tile = request(url);
-          tile.pipe(res);
-          return;
-          request(url, {}, (err, response, body) => {
-            console.log(err, response.statusCode);
-            res.setHeader("Content-Type", "image/png");
-            res.status(response.statusCode).send(body);
-          });
-          /*
-
-        proxy.web(
-          req,
-          res,
-          {
-            target: "https://b.tile.openstreetmap.org",
-            secure:true
-          },
-          e => console.log(e)
-        );*/
-        }
-      );
+        https.get(
+          `https://${p.s}.tile.openstreetmap.org/${p.z}/${p.x}/${p.y}.png`,
+          mapResponse => {
+            mapResponse.pipe(res);
+          }
+        );
+      });
 
       // assets for frontend
       app.get("/*", express.static("dist", { redirect: false }));
